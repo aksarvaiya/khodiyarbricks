1eadb6f35435d1130a5d90aae593b53e65f25965
Ankit Sarvaiya <ankit.s@strica.io>
2025-11-23 08:32:46 +0530

    Initial commit: Khodiyar Bricks coming soon site

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// src/App.jsx
+import ComingSoon from "./ComingSoon";
+import "./ComingSoon.css";
+
+function App() {
+  return <ComingSoon />;
+}
+
+export default App;
---
+++ src/ComingSoon.jsx
@@ -0,0 +1,17 @@
+import "./ComingSoon.css";
+
+export default function ComingSoon() {
+  return (
+    <div className="coming-wrapper">
+      <div className="coming-inner">
+        <img src="/logo.png" alt="Khodiyar Bricks Logo" className="logo" />
+
+        <h1>Coming Soon</h1>
+
+        <p>
+          Our official website is under construction. Stay tuned for updates!
+        </p>
+      </div>
+    </div>
+  );
+}
